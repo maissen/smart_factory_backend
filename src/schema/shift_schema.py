10851493f2c9b037ba15e7a4cc82ff7f28aaa7cd
Maissen <maissen.developer500@gmail.com>
2025-11-08 10:12:39 +0100

    Create shift schema

---
+++ src/schema/shift_schema.py
@@ -0,0 +1,19 @@
+from datetime import time
+from pydantic import BaseModel
+
+
+class ShiftCreateSchema(BaseModel):
+    name: str
+    start_time: time
+    end_time: time
+
+
+class ShiftResponseSchema(BaseModel):
+    id: int
+    factory_id: int
+    name: str
+    start_time: time
+    end_time: time
+
+    class Config:
+        from_attributes = True
--- src/services/shifts/create_shift_service.py
+++ src/services/shifts/create_shift_service.py
@@ -8,6 +8,7 @@ from src.models.shifts_model import Shift
 
 from src.db_crud.shift.create_shift_crud import create_shift_crud
 from src.db_crud.shift.get_shift_crud import get_shift_by_factory_id_crud
+from src.services.shifts.get_shift_service import check_if_factory_has_shift_service
 
 from src.exceptions.shifts_exceptions import (
     ShiftNotFoundError,
@@ -32,9 +33,9 @@ def create_shift_service(
 
     assert_factory_access(factory, current_user)
 
-    existing = get_shift_by_factory_id_crud(db, factory_id)
+    existing = check_if_factory_has_shift_service(db=db, factory_id=factory_id, current_user=current_user)
     if existing:
-        raise ShiftAlreadyExistsError(factory_id)
+        raise ShiftAlreadyExistsError("Factory already has a shift.")
 
     validate_time_range(start_time, end_time)
 
