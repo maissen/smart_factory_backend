bb920b6f4586ead75e3afc5a40d87fa6a5ab44c1
Maissen <maissen.developer500@gmail.com>
2025-11-09 13:19:27 +0100

    Enhance update machine route.

--- src/services/machine/get_machine_service.py
+++ src/services/machine/get_machine_service.py
@@ -1,16 +1,14 @@
 from sqlalchemy.orm import Session
-from sqlalchemy.exc import IntegrityError
 
 from src.db_crud.machine.machine_crud import get_factory_machines_crud, get_machine_by_name_crud, get_machine_by_serial_crud
 from src.exceptions.machine_exceptions import (
     MachineFetchError,
     MachineNotFoundError
 )
-from src.exceptions.user_exceptions import UserAuthorizationError
-
-from src.helpers.str_helpers import is_valid_str, normalize_str
 from src.services.factory.get_factory_service import get_factory_by_id_service
 from src.services.user.get_user_by_id_service import get_user_by_id_service
+from src.helpers.factory import validate_factory_id
+from src.helpers.validate_user_id import validate_user_id
 
 from src.core.settings import settings
 
@@ -18,20 +16,19 @@ from src.core.settings import settings
 def get_all_factory_machines_service(db: Session, user_id: int, factory_id: int = None):
     """Get all machines."""
 
+    validate_user_id(user_id)
+    validate_factory_id(factory_id)
+
     # check if factory exists
     factory = get_factory_by_id_service(db=db, factory_id=factory_id)
 
     # check if user exists
     user = get_user_by_id_service(db=db, user_id=user_id)
 
-    # check if user is authorized
-    if not factory.owner_id != user_id and user.role != settings.USER_ALLOWED_ROLES[0]: #admin
-        raise UserAuthorizationError()
-
     try:
         machines = get_factory_machines_crud(db=db, factory_id=factory_id)
     except:
-        raise MachineFetchError()
+        raise MachineFetchError("Failed to fetch machines of the factory.")
 
     return machines
 
--- src/services/machine/update_machine_service.py
+++ src/services/machine/update_machine_service.py
@@ -1,20 +1,12 @@
 from sqlalchemy.orm import Session
-from sqlalchemy.exc import IntegrityError
-
 from src.db_crud.machine.machine_crud import update_machine_crud
-from src.exceptions.machine_exceptions import (
-    MachineError,
-    MachineNotFoundError,
-    MachineNameAlreadyExistsError,
-    MachineSerialNumberAlreadyExistsError,
-    InvalidMachineStatusError,
-    MachineAccessDeniedError,
-)
-from src.exceptions.user_exceptions import UserAuthorizationError
+from src.exceptions.machine_exceptions import *
 from src.helpers.str_helpers import is_valid_str, normalize_str
 from src.services.factory.get_factory_service import get_factory_of_user_service
 from src.services.machine.get_machine_service import get_all_factory_machines_service, get_machine_by_serial_service, get_machine_by_name_service
 from src.core.settings import settings
+from src.helpers.str_helpers import is_valid_str
+from src.helpers.validate_user_id import validate_user_id
 
 
 def update_machine_service(
@@ -28,10 +20,10 @@ def update_machine_service(
     description: str = None
 ):
     """Service to update an existing machine by ID with all fields."""
+
+    validate_user_id(user_id)
     
     factory = get_factory_of_user_service(db=db, user_id=user_id)
-    if factory.owner_id != user_id:
-        raise UserAuthorizationError()
 
     machines = get_all_factory_machines_service(db=db, factory_id=factory.id, user_id=user_id)
     machine = None
@@ -45,19 +37,18 @@ def update_machine_service(
     if machine.factory_id != factory.id:
         raise MachineAccessDeniedError()
 
-    if not is_valid_str(name):
-        raise MachineError()
+    is_valid_str(name, raise_on_error=True, err_msg="Machine name must be a non-empty valid name.")
     name = normalize_str(name)
-    machine_by_name = get_machine_by_name_service(db=db, name=name, raise_if_not_found=False)
+
+    machine_by_name = get_machine_by_name_service(db=db, name=name, raise_err=False)
     if machine_by_name:
         raise MachineNameAlreadyExistsError()
 
-    if not is_valid_str(serial_number):
-        raise MachineError("Serial number is required.")
+    is_valid_str(serial_number, raise_on_error=True, err_msg="Machine serial number must be a non-empty valid string.")
     serial_number = normalize_str(serial_number)
-    machine_by_serial_nb = get_machine_by_serial_service(db=db, serial_number=serial_number, raise_if_not_found=False)
+    machine_by_serial_nb = get_machine_by_serial_service(db=db, serial_number=serial_number, raise_err=False)
+    
     if machine_by_serial_nb:
-        print('machine is found')
         raise MachineSerialNumberAlreadyExistsError()
 
     if status not in settings.MACHINE_POSSIBLE_STATUS:
@@ -75,7 +66,8 @@ def update_machine_service(
         )
 
     except Exception as e:  
-        raise MachineError()
+        print(e)
+        raise MachineError("Failed to update machine.")
 
     return updated_machine
 
