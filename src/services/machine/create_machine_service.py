f59253fa2ad15bd66e0d88245108a31fdf3a441e
Maissen <maissen.developer500@gmail.com>
2025-11-09 16:07:43 +0100

    Rollback vm creation with docker

--- src/services/machine/create_machine_service.py
+++ src/services/machine/create_machine_service.py
@@ -5,7 +5,6 @@ from src.exceptions.machine_exceptions import *
 from src.helpers.str_helpers import is_valid_str, normalize_str
 from src.services.machine.get_machine_service import get_machine_by_serial_service, get_machine_by_name_service
 from src.services.factory.get_factory_service import get_factory_of_user_service
-from src.services.machine.launch_vm_as_container import launch_vm_container
 from src.core.settings import settings
 
 
@@ -52,9 +51,6 @@ def create_machine_service(
             last_maintenance_date=last_maintenance_date,
             description=description
         )
-        
-        # create the vm agent using docker
-        # launch_vm_container(machine_id=new_machine.id)
         
         return new_machine
         
